d73399a10f74884cb13b6c8e0f191e1a1f78efbb
luiscbedoya <bedoyaandocillaluiscarlos@gmail.com>
2024-04-30 17:28:25 -0500

    add install.sh

--- install.sh
+++ install.sh
@@ -22,56 +22,54 @@ else
 	exit 1
 fi
 
-# paquetes xorg 
-$is xorg xorg-xinit xorg-server-xdmx xorg-xsetroot xdo xorg-xbacklight
-# Compilacion, windows, red y demas
-$is ncurses openssl dkms lz4 bc rsync linux-zen-headers ninja fuse cmake git python-pip libglvnd at-spi2-core base-devel p7zip ntp ntfs-3g exfat-utils pciutils lshw network-manager-applet mtools dosfstools gvfs wget curl unzip tar dbus ripgrep python-virtualenv glow
-# intel 
-$is xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa
-$is libva-intel-driver libva-utils vdpauinfo clinfo
-$is intel-compute-runtime
-#nvidia
-$is nvidia-dkms nvidia-settings cuda nvidia-utils lib32-nvidia-utils opencl-nvidia  lib32-opencl-nvidia vdpauinfo clinfo
-#audio
-$is pulsemixer pipewire pipewire-pulse wireplumber alsa-utils
-#navegador and fonts
-$is chromium noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
-#wallpapers
-$is nitrogen
-#notificaciones
-$is dunst libnotify
-# numkeys
-$is numlockx
-# explorador de archivos
-$is pcmanfm
-# lector de pdf 
-$is zathura
-#imagenes
-$is phototonic
-# capturas de pantalla
-$is flameshot
-#editor de texto
-$is leafpad
-# reproductor de audio y video
-$is mpv
-# administrador de pantalla
-$is arandr
-#terminal
-$is kitty
-#screen record
-$is simplescreenrecorder
-#info terminal
-$is neofetch
-# temas
-$is lxappearance
-#bluetooth
-#$is bluez bluez-libs bluez-utils bluez-openrc blueman
-# paint
-$is pinta
-#Android Debug Bridge
-$is android-tools
-#pass
-$is keepassxc
-#vol
-$is pavucontrol
+clear
+echo "Installing graphics and audio..."
+sleep 2s
+
+#### Xorg Packages
+$is xorg-server xorg-xinit
+
+#### Intel Packages
+$is vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo intel-compute-runtime 
+
+#### Nvidia Packages
+$is nvidia nvidia-settings nvidia-utils lib32-nvidia-utils cuda opencl-nvidia  lib32-opencl-nvidia
+
+#### Audio
+$is pulseaudio 
+
+clear
+echo "Installing base packages..."
+sleep 2s
+
+$is at-spi2-core base-devel ntfs-3g gvfs ntp unzip tar wget curl dbus polkit-gnome feh git libnotify xfce4-notifyd xdo dosfstool mtools cmake ninja
+
+clear
+echo "Installing useful packages ..."
+sleep 2s
+
+$is ripgrep glow python-virtualenv python-pip 
+
+clear
+echo "Installing user packages..."
+sleep 2s
+$is noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
+$is kitty pcmanfm leafpad zathura lxappearance flameshot viewnior neovim
+
+#### Config AutoStart
+
+echo -e '#autostart
+\nif [ -d "$HOME/.local/bin" ] ; then
+\n    PATH="$HOME/.local/bin:$PATH"
+\nfi
+\nsetxkbmap latam &
+\nexec dbus-run-session i3'> $HOME/.xinitrc 
+
+###
+clear
+echo "Instalación de VoidBsp-term Completada"
+echo "Pulse cualquiera tecla para continuar..."
+read tecla
+clear
+$s reboot
 
